e981ae02c63fc366c0a7644fd9f6b3c4ac26f31e
raafarosa <141688193+raafarosa@users.noreply.github.com>
2026-04-16 16:29:37 -0300

    Redução de cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'plantao-pro-v1';
+// Sempre que fizer uma atualização importante, mude a versão aqui (v1 -> v2...)
+const CACHE_NAME = 'plantao-pro-v1.1'; 
 const assets = [
   './',
   './index.html',
@@ -8,18 +9,39 @@ const assets = [
   './manifest.json'
 ];
 
+// Instalação: Salva os arquivos essenciais
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Força a atualização imediata
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('SW: Cache Instalado');
       return cache.addAll(assets);
     })
   );
 });
 
+// Ativação: Limpa o lixo (caches antigos)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('SW: Removendo cache antigo:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Busca (Fetch): Estratégia Network-First
+// Ele tenta a rede. Se falhar (offline), pega no cache.
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
